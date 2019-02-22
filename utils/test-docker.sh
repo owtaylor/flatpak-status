ab8f83ebb74c6190ae8ff07eb9072b48a27168eb
Owen W. Taylor <otaylor@fishsoup.net>
2019-02-22 06:35:13 +0000

    Add a travis.yml and a test-docker.sh
    Add a travis build configuration that builds the project inside a
    Docker container.

---
+++ utils/test-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+docker build --no-cache -t flatpak-status:latest .
+./utils/update-test-data.sh --only-fetch-cache
+docker run \
+       --rm \
+       -v $(pwd)/cache:/opt/flatpak-status/cache \
+       -v $(pwd)/.test-data:/opt/flatpak-status/.test-data \
+       -v $(pwd)/test-data:/opt/flatpak-status/test-data \
+       flatpak-status:latest \
+       pipenv run ./utils/test.sh --update-test-data --no-fetch-cache
