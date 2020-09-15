26adbf3b5d638d872cfa8dc9c19832d5ada6394f
Owen W. Taylor <otaylor@fishsoup.net>
2020-09-15 11:05:27 -0400

    release_info: update for F33 branched, F30 EOL

--- flatpak_status/release_info.py
+++ flatpak_status/release_info.py
@@ -22,8 +22,9 @@ releases = [
     Release(name='F27', branch='f27',    tag='f27', status=ReleaseStatus.EOL),
     Release(name='F28', branch='f28',    tag='f28', status=ReleaseStatus.EOL),
     Release(name='F29', branch='f29',    tag='f29', status=ReleaseStatus.EOL),
-    Release(name='F30', branch='f30',    tag='f30', status=ReleaseStatus.GA),
+    Release(name='F30', branch='f30',    tag='f30', status=ReleaseStatus.EOL),
     Release(name='F31', branch='f31',    tag='f31', status=ReleaseStatus.GA),
     Release(name='F32', branch='f32',    tag='f32', status=ReleaseStatus.GA),
-    Release(name='F33', branch='master', tag='f33', status=ReleaseStatus.RAWHIDE),
+    Release(name='F33', branch='f33',    tag='f33', status=ReleaseStatus.BRANCHED),
+    Release(name='F34', branch='master', tag='f34', status=ReleaseStatus.RAWHIDE),
 ]
