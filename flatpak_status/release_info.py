52bcde7572768438ea525be0985179d917bb1e66
Owen W. Taylor <otaylor@fishsoup.net>
2021-11-11 23:13:15 -0500

    release_info.py: update for F35 GA

--- flatpak_status/release_info.py
+++ flatpak_status/release_info.py
@@ -27,6 +27,6 @@ releases = [
     Release(name='F32', branch='f32',     tag='f32', status=ReleaseStatus.EOL),
     Release(name='F33', branch='f33',     tag='f33', status=ReleaseStatus.GA),
     Release(name='F34', branch='f34',     tag='f34', status=ReleaseStatus.GA),
-    Release(name='F35', branch='f35',     tag='f35', status=ReleaseStatus.BRANCHED),
+    Release(name='F35', branch='f35',     tag='f35', status=ReleaseStatus.GA),
     Release(name='F36', branch='rawhide', tag='f36', status=ReleaseStatus.RAWHIDE),
 ]
